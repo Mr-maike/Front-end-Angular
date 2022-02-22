d7af55150b9fab0b07dede8bdfae3d3c644f20ef
ADM <darkgaroto@gmail.com>
2022-02-22 00:05:50 -0300

    :lipstick: Update Design

--- src/app/languages/languages.component.ts
+++ src/app/languages/languages.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import jobsdata from '../api/jobs.json';
+
+interface Trabalho{
+  id: number;
+  linguagem: string;
+  frameworkFront: string;
+  frameworkBack: string;
+  vaga: string;
+  descricao: string;
+  salario: number;
+}
 
 @Component({
   selector: 'app-languages',
@@ -11,5 +22,7 @@ export class LanguagesComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  jobs:Trabalho[] = jobsdata;
 
 }
